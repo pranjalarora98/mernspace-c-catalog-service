4b7ed114d7ea353afb684ee52539d95c59a96dd4
Rakesh K <codersgyan@gmail.com>
2024-01-27 01:12:52 +0300

    improve error logging

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,12 @@
-import express, { NextFunction, Request, Response } from "express";
-import logger from "./config/logger";
-import { HttpError } from "http-errors";
+import express, { Request, Response } from "express";
+import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to Auth service");
+    res.send("Hello World!");
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message);
-    const statusCode = err.statusCode || err.status || 500;
-    res.status(statusCode).json({
-        errors: [
-            {
-                type: err.name,
-                msg: err.message,
-                path: "",
-                location: "",
-            },
-        ],
-    });
-});
+app.use(globalErrorHandler);
 
 export default app;
